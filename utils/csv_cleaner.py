eb7992d94a365cf18bcc026f0af3a9a2824f41bc
Explorer <cometajay21@gmail.com>
2020-09-05 14:21:03 +0530

    merged master

--- utils/csv_cleaner.py
+++ utils/csv_cleaner.py
@@ -3,22 +3,22 @@ import os
 
 rowData = []
 path = os.path.dirname(os.getcwd())
-with open(path+'/dataset/MSR Video Description Corpus.csv', 'r',encoding="utf8") as file:
+with open(path+'/dataset/MSR Video Description Corpus.csv', 'r', encoding="utf8") as file:
     reader = csv.reader(file)
     for row in reader:
         tempRow = []
-        if len(row)>0 and row[6]=='English':
+        if len(row) > 0 and row[6] == 'English':
             tempRow.append(row[0]+'_'+row[1]+'_'+row[2]+'.avi')
             tempRow.append(row[7])
-        if len(tempRow)==2:
+        if len(tempRow) == 2:
             rowData.append(tempRow)
 
-#print(rowData)
+# print(rowData)
 
 fields = ['VideoID', 'Description']
 
 # writing to csv file
-with open(path+'/dataset/MSVD_description_cfile.csv', 'w',encoding="utf8") as csvfile:
+with open(path+'/dataset/MSVD_description_cfile.csv', 'w', encoding="utf8") as csvfile:
     csvwriter = csv.writer(csvfile)
     csvwriter.writerow(fields)
     csvwriter.writerows(rowData)
